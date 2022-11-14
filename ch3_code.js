eab6fdba29c379007bf78c9c91afd67e4a831088
Jephtha Tandri <88237760+Jephtha-T@users.noreply.github.com>
2022-11-14 20:23:47 +0700

    Add files via upload

---
+++ ch3_code.js
@@ -0,0 +1,4 @@
+var headingtext = "<h1> A Page of Javascript</h1>" + 
+	"Hello, welcome to my Javascript page 1" + 
+	"<a href =\"http:/yahoo.com\">link to yahoo</a>";
+document.write(headingtext);
---
+++ ch3_code2.js
@@ -0,0 +1,6 @@
+var headingtext = "<h1> A Page of Javascript</h1>";
+var textname = "Hello, welcome to my Javascript page 1";
+var linkname = "<a href =\"http:/yahoo.com\">link to yahoo</a>";
+document.write(headingtext);
+document.write(textname);
+document.write(linkname);
